57ca8e6068e9b3999e52098b28e270cc8cc23ded
Rahul Joshi <jimi.rahul@gmail.com>
2018-08-26 19:03:48 +0530

    hashing passwords before they are saved to db

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,22 @@
 const { SHA256 } = require('crypto-js');
 const jwr = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let message = 'Rahul Joshi';
-let hashedMessage = SHA256(message).toString();
+let password = 'abc123!';
 
-console.log(hashedMessage);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
+
+let hashedPassword = '$2a$10$L0MTqkwrTR0s.xQTYCl15uBDUjNgzDBymFKE8Ohq.HR/6ICEe4S4y';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+// let message = 'Rahul Joshi';
+// let hashedMessage = SHA256(message).toString();
+
+// console.log(hashedMessage);
