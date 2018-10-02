7e5aeef7e964fa07b3a02ca9f790be4222bd1810
Rahul Joshi <jimi.rahul@gmail.com>
2018-10-02 18:39:09 +0530

    Added async and await

--- server/server.js
+++ server/server.js
@@ -59,24 +59,25 @@ app.get('/todos/:id', authenticate, (req, res) => {
 	});
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-	const todoId = req.params.id;
-	if(!ObjectId.isValid(todoId)) {
-		return res.status(404).send({
-			msg: 'Invalid object id'
+app.delete('/todos/:id', authenticate, async (req, res) => {
+	try {
+		const todoId = req.params.id;
+		if(!ObjectId.isValid(todoId)) {
+			return res.status(404).send({
+				msg: 'Invalid object id'
+			});
+		}
+		const todo = await Todo.findOneAndRemove({
+			_id: todoId,
+			_creator: req.user._id
 		});
-	}
-	Todo.findOneAndRemove({
-		_id: todoId,
-		_creator: req.user._id
-	}).then((todo) => {
 		if (todo) {
 			return res.send({todo});
 		}
 		return res.status(404).send();
-	}).catch((e) => {
+	} catch (e) {
 		res.status(400).send();
-	});
+	}
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
@@ -113,61 +114,41 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 	});
 });
 
-app.post('/users', (req, res) => {
-	const body = _.pick(req.body, ['email', 'password']);
-	const user = new User(body);
-	user.save().then(() => {
-		return user.generateAuthToken();
-	}).then((token) => {
+app.post('/users', async (req, res) => {
+	try {
+		const body = _.pick(req.body, ['email', 'password']);
+		const user = new User(body);
+		await user.save();
+		const token = await user.generateAuthToken();
 		res.header('x-auth', token).send(user);
-	}).catch((e) => {
+	} catch (e) {
 		res.status(400).send(e);
-	});
+	}
 });
 
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-	const body = _.pick(req.body, ['email', 'password']);
-
-	User.findByCredentials(body.email, body.password).then((user) => {
-		//return res.send(user);
-		return user.generateAuthToken().then((token) => {
-			res.header('x-auth', token).send(user);
-		}).catch((e) => {
-			res.status(400).send(e);
-		});
-	}).catch((e) => {
+app.post('/users/login', async (req, res) => {
+	try {
+		const body = _.pick(req.body, ['email', 'password']);
+		const user = await User.findByCredentials(body.email, body.password);
+		const token = await user.generateAuthToken();
+		res.header('x-auth', token).send(user);
+	} catch (e) {
 		res.status(400).send();
-	});
-	// User.findOne({email: body.email}).then((user) => {
-	// 	if (user) {
-	// 		let hashedPassword = user.password;
-	// 		return bcrypt.compare(body.password, hashedPassword, (err, result) => {
-	// 			if (err) {
-	// 				return res.status(400).send({
-	// 					msg: 'Wrong Password!'
-	// 				});
-	// 			}
-	// 			if (result) {
-	// 				return res.send(user);
-	// 			}
-	// 			return res.status(400).send();
-	// 		});
-	// 	}
-	// 	return res.status(400).send();
-	// });
+	}
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-	req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+	try {
+		await req.user.removeToken(req.token);
 		res.status(200).send();
-	}).catch((e) => {
+	} catch (e) {
 		res.status(400).send();
-	})
-})
+	}
+});
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
